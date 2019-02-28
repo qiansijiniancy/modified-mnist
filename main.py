2f44b3baa49a8edaccbecd9939f599d0fe6d49ed
qiansijiniancy <sdyang522@gmail.com>
2019-02-28 10:21:04 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,57 @@
+###  Mini proj 3
+### McGill University
+### Sidi Yang
+
+'''
+Step 1: Load Data
+'''
+
+# This Python 3 environment comes with many helpful analytics libraries installed
+# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
+# For example, here's several helpful packages to load in
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+# Input data files are available in the "../input/" directory.
+# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+
+import os
+cwd = os.getcwd()
+input_path = cwd+"/data"
+print(os.listdir(input_path))
+
+# Any results you write to the current directory are saved as output.
+
+
+#Files are stored in pickle format.
+#Load them like how you load any pickle. The data is a numpy array
+import pandas as pd
+train_images = pd.read_pickle(cwd + '/data/train_images.pkl')
+train_labels = pd.read_csv(cwd +'/data/train_labels.csv')
+test_images = pd.read_pickle(cwd + '/data/test_images.pkl')
+
+print(train_images.shape)
+print(train_labels.shape)
+
+import matplotlib.pyplot as plt
+
+#Let's show image with id 16
+img_idx = 13
+
+plt.title('Label: {}'.format(train_labels.iloc[img_idx]['Category']))
+plt.imshow(train_images[img_idx])
+plt.show()
+
+'''
+Step 2: Train models
+'''
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.data
+from torch.autograd import Variable
+import tensorflow as tf
+
+from sklearn.model_selection import train_test_split
+
